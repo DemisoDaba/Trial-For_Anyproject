9a894569d68aff1d365f1812d2543ac186586445
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-04-22 13:55:46 +0300

    Update handlers.c

--- Check_Code/handlers.c
+++ Check_Code/handlers.c
@@ -3,7 +3,7 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * by Joseph and Ciny
+ * by Demiso and 
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
