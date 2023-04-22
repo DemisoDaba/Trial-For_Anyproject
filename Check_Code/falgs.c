9a6748d3ffa76d3f014ba7c2ce52ab2165c3bd6d
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-04-22 13:54:16 +0300

    Update falgs.c

--- Check_Code/falgs.c
+++ Check_Code/falgs.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * by Joseph and Ciny
+ * by Demiso and 
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
