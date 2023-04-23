343884a1d43dc71a1de9affea4a804ce7abdd9e2
DemisoDaba <demo.nkmt1@gmail.com>
2023-04-23 01:22:29 -0700

    Demiso

---
+++ Demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int today_self = 10;
+    int yesterday_self = 8;
+
+    if (today_self > yesterday_self) 
+    {
+        printf("You are on the right path\n");
+    }
+
+    return 0;
+}
