0fd11494778185f771e2684d803453124045d188
impactnelson <nghonelson14@gmail.com>
2023-04-24 03:11:47 -0700

    prnt_precision.c update

--- prnt_precision.c
+++ prnt_precision.c
@@ -9,23 +9,4 @@
 */
 
 char *get_precision(char *s, params_t *params, va_list ap)
-{
-	int d = 0;
-
-	if (*s != '.')
-		return (s);
-	s++;
-	if (*s == '*')
-	{
-		d = va_arg(ap, int);
-		s++;
-	}
-	else
-	{
-		while (_isdigit(*s))
-			d = d * 10 + (*s++ - '0');
-	}
-	params->precision = d;
-	return (s);
-}
 
